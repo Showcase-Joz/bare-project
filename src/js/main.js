2ee729a994ea02a98d54db9886a693afb0e94a80
Joz <sunrise.joz77@gmail.com>
2019-10-18 12:28:01 +0100

    updated git and build process

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+/*
+  Author Name: Joz
+  Author URI: https://sst-l.com
+  Note: 
+
+  -- functions
+
+  ++Do not remove the back-link in this web template
+
+
+
+  -------------------------------------------------------
+*/
+
+
+/* FUNCTIONS
+ ************************
+ ************************/
+
+
+
+/* ****************** UI Manipulation ****************** */
